ca1ded0a141b56cd8ec69f9ace91acffb9d31c0b
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2023-06-14 16:50:52 -0400

    added custom log middleware to store any issues that may arise and be sent to the logs with date and time if/when it rises

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
 
+app.use(express.json())
+
 app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.use('/', require('./routes/root'))
@@ -12,7 +14,7 @@ app.all('*', (req, res) => {
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if (req.accepts('json')) {
-        res.json({ message: '404 not found!' })
+        res.json({message: '404 not found!'})
     } else {
         res.type('txt').send('404 not found!')
     }
