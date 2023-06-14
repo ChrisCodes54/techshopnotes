449570ac00e31123803a300878adaa257c4b0bcd
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2023-06-14 17:17:06 -0400

    finalized error handler, error log, allowed origins access, corsoptions, and finished middleware tbd

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhose:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com'
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorhandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
 
+app.use(logger)
+
+app.use(cors(corsOptions))
+
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 
@@ -14,10 +25,12 @@ app.all('*', (req, res) => {
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if (req.accepts('json')) {
-        res.json({message: '404 not found!'})
+        res.json({ message: '404 not found!' })
     } else {
         res.type('txt').send('404 not found!')
     }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`))
