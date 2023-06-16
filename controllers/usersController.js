514dc68243a27a273c48bf4e5cef53a5a796c996
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2023-06-16 17:08:22 -0400

    getting error when testing /users, User.find is not a function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,12 +2,13 @@ const User = require('../models/User')
 const Note = require('../models/Note')
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
-const { response } = require('express')
 
 
 //GET method
 const getAllUsers = asyncHandler(async (req, res) => {
+
     const users = await User.find().select('-password').lean()
+
     if(!users) {
         return res.status(400).json({message: 'No users found'})
     } 
@@ -46,11 +47,62 @@ return res.status(400).json({message: 'All fields are required'})
 
 //patch method
 const updateUser = asyncHandler(async (req, res) => {
+    const {id, username, roles, active, password} = req.body
 
+    if(!id || !username || !Array.isArray(roles) || typeof active !== 'boolean') {
+        return res.status(400).json({message:'All fields are required'})
+    }
+
+    const user = await user.findById(id).exec()
+
+    if(!user) {
+        return res.status(400).json({message: 'User not found'})
+    }
+
+    //checking for any duplicate
+    const duplicate = await User.findOne({username}).lean().exec()
+
+    if(duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({message: 'Duplicate username!'})
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+
+        if(password) {
+            //hashing the passoword
+            user.password = await bcrypt.hash(password, 10)
+        }
+
+        const updatedUser = await user.save()
+        res.json({message: `${updatedUser.username} updated`})
 })
 
 //delete method
 const deleteUser = asyncHandler(async (req, res) => {
+    const {id} = req.body 
+
+    if(!id) {
+        return res.status(400).json({message:'User ID Required!'})
+    }
+
+    const notes = await Note.findOne({user: id}).lean().exec()
+
+    if(notes?.length) {
+        return res.status(400).jsom({message: 'User has assigned notes'})
+    } 
+
+    const user = await user.findById(id).exec()
+    if(!user) {
+        return res.status(400).json({message: 'User not found'})
+    }
+
+    const result = await user.deleteOne() 
+    
+    const reply = `Username ${result.username} with ID ${result._id} deleted!`
+
+    response.json(reply)
 
 })
 
