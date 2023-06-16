3be3734cdcbb957fbe0506bc5805441e434fd57e
Christopher Tangarife <christophertangarife@christophersmbp.lan>
2023-06-16 10:09:14 -0400

    finised structure for CRUD on usercontrollers, needs logic in each method

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,3 +2,32 @@ const User = require('../models/User')
 const Note = require('../models/Note')
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
+
+
+//GET method
+const getAllUsers = asyncHandler(async (req, res) => {
+
+})
+
+//POST method
+const createNewUser = asyncHandler(async (req, res) => {
+
+})
+
+
+//patch method
+const updateUser = asyncHandler(async (req, res) => {
+
+})
+
+//delete method
+const deleteUser = asyncHandler(async (req, res) => {
+
+})
+
+module.exports = {
+    getAllUsers,
+    createNewUser,
+    updateUser,
+    deleteUser
+}
