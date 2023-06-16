a1feb8a2dbf9a55566191662310ae81df2c83a4f
Christopher Tangarife <christophertangarife@christophersmbp.lan>
2023-06-16 10:24:27 -0400

    added methods to userroutes.js and added logic for getAllUsers, still need to complete createNewUser and rest

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,16 +2,25 @@ const User = require('../models/User')
 const Note = require('../models/Note')
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
+const { response } = require('express')
 
 
 //GET method
 const getAllUsers = asyncHandler(async (req, res) => {
-
+    const users = await User.find().select('-password').lean()
+    if(!users) {
+        return res.status(400).json({message: 'No users found'})
+    } 
+    res.json(users)
 })
 
 //POST method
 const createNewUser = asyncHandler(async (req, res) => {
+    const {username, password, roles}= req.body
 
+    if(!username || !password || !Array.isArray(roles) || !roles.length) {
+return res.status(400).json({message: 'All fields are required'})
+    }
 })
 
 
